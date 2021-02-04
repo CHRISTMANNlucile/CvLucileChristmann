0d8b7411a1c5d5181e0e208cdeb00aba2eac142c
luz <luz@macbook-air-de-luz.home>
2021-02-04 16:41:55 +0100

    2

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+require_once("head.html");
+?>
+<body>
+
+  <h1 class="site-heading text-center text-white d-none d-lg-block">
+    <span class="site-heading-upper text-primary mb-3">lucile christmann</span>
+    <span class="site-heading-lower">développeuse web</span>
+  </h1>
+
+<?php
+require_once("menu.html");
+?>
+</br>
+<div class="about-heading-content">
+  <div class="row">
+    <div class="col-xl-9 col-lg-10 mx-auto">
+      <div class="bg-faded rounded p-5">
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-upper">compétences</span>
+          <span class="section-heading-lower">front</span>
+        </h2>
+        <p><em>Html - Css - Javascript - Bootstrap - NodeJs - Npm - Photoshop - Wordpress</em></p>
+        <p class="mb-0">Conception et développement front-end d'interfaces web avec ou sans CMS</em></p>
+        <h2 class="section-heading mb-4">
+</br>
+          <span class="section-heading-lower">BACK</span>
+        </h2>
+        <p><em>Apache - MySQL - Php - MySQL Workbench - PhpMyAdmin</em> </p>
+        <p class="mb-0">Développement back-end d'une application web - Conception, gestion de base de données - Développement des composants d'accès à une base de données - Programmation défensive et sécurisation - Optimisation, compilation des codes - Respect du PSR </em></p>
+        <h2 class="section-heading mb-4">
+</br>
+<hr>
+        </h2>
+        <p>anglais professionnel</p>
+        <p class="mb-0">Configurations et maintenance en lignes de commande - versionning (Github) - mise en production - environnements MacOs et Windows 10</p>
+      </div>
+    </div>
+  </div>
+</div>
+</br>
+
+ 
+<div class="about-heading-content">
+  <div class="row">
+    <div class="col-xl-9 col-lg-10 mx-auto">
+      <div class="bg-faded rounded p-5">
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-upper">expériences web</span>
+          <span class="section-heading-lower">réalisations</span>
+        </h2>
+        <p>- Création d'une application ( <B>Back-end</b> / <b>Front-end</b> ) permettant d'<b>ajouter</b>, de <b>modifier</b> ou de <b>supprimer</b> des actions de formation à la <b>base de données</b> du Livret Numérique du <b>GRETA</b> de la Loire.</br>
+               - <b>Migration mysql_connect</b> ( <em>PHP 5.6.4</em> ) => <b>PDO</b> ( <em>PHP 7.4.3</em> ) du site GenAc du GRETA</br>
+               - <b>Refonte</b> de la <b>base de données</b> et <b>maquettage</b> en vue d'un projet d'extension du site GenAc</br>
+               - Création d'un site <b>WORDPRESS</b> / <b>Plugin</b> permettant d'afficher les données d'une <b>APIweb</b> en <b>AJAX</b> </p>
+        <p class="mb-0"></em></p>
+      </div>
+    </div>
+  </div>
+</div>
+</br>
+
+<div class="about-heading-content">
+  <div class="row">
+    <div class="col-xl-9 col-lg-10 mx-auto">
+      <div class="bg-faded rounded p-5">
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-upper">expériences professionneles</span>
+          <span class="section-heading-lower">web</span>
+        </h2>
+        <p><b>2020</b> Stage développeuse web au <b>GRETA de Loire</b> - 2 mois - <em>Saint-Etienne</em></b></br>
+           <b>2019</b> Stage développeuse web à <b>TORIGAMI</b> - 2 semaines - <em>Saint-Etienne</em></b>
+        </p>
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-lower">rénovation</span>
+        </h2>
+        <p><b>2013 - 2021</b> Chantiers de rénovation (auto-entrepreneur) - Interim</br>
+        <b>2012 - 2013</b> Rénovation, décoration chez Art d'Eco et Environnement - <em>Grenoble</em></br>
+        <b>2011</b> Peinture chez Goumas - <em>Chambéry</em></br>
+        <b>2009 - 2010</b> Façades et colombages - <em>Moliets & Maâ</em>
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-lower">enfance</span>
+        </h2>
+        <p><b>2015 -2018</b> AVS en classe ULIS - <em>Saint Etienne</em></br>
+        <b>2003 - 2015</b> Animation - <em>Lyon / Saint-Chamond</em></p>
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-lower">audiovisuel</span>
+        </h2>
+        <p>Cadrage et montage de court-Métrages documentaires</br>
+        <b>2004</b> Stage aux Studios Robert Bouquet - <em>Saint-Etienne</em></br>
+        <b>2003</b> Stage à France 3 Production - <em>Lilles</em></br>
+        <b>2001</b> Stage à France 3 Rhône Alpes - <em>Lyon</em></p>
+      </div>
+    </div>
+  </div>
+</div>
+</br>
+
+<div class="about-heading-content">
+  <div class="row">
+    <div class="col-xl-9 col-lg-10 mx-auto">
+      <div class="bg-faded rounded p-5">
+        <h2 class="section-heading mb-4">
+          <span class="section-heading-upper">études et formations</span>
+          <span class="section-heading-lower">diplômes</span>
+        </h2>
+        <p><b>2020 Titre Professionnel Développeur Web / web mobile</b></br>
+        <b>2015</b> CAP Petite enfance</br>
+        <b>2008</b> CAP Peintre en bâtiment</br>
+        <b>2004 BTS Audiovisuel Montage</b></br>
+        <b>2002</b> L1 Médiation Culturelle et Communication</br>
+        <b>2001</b> Bac L Audiovisuel
+
+  
+
+        </p>
+        <p class="mb-0"></em></p>
+      </div>
+    </div>
+  </div>
+</div>
+</br>
+
+
+
+<?php
+require_once("footer.html");
+?>
+</body>
+</html>
---
+++ xdream.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+require_once("head.html");
+?>
+<body>
+  <h1 class="site-heading text-center text-white d-none d-lg-block">
+    <span class="site-heading-lower">site wordpress</span>
+  </h1>
+
+<?php
+require_once("menu.html");
+?>
+  <section class="page-section about-heading">
+    <div class="container">
+      <center><img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/xdream/xHome.png" alt=""></center>
+    </div>
+  </section>
+
+  <section class="page-section about-heading">
+    <div class="container">
+      <center><img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/xdream/xGalery.png" alt=""></center>
+    </div>
+  </section>
+
+  <section class="page-section about-heading">
+    <div class="container">
+      <center><img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/xdream/xMessage.png" alt=""></center>
+    </div>
+  </section>
+
+  <section class="page-section about-heading">
+    <div class="container">
+      <center><img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/xdream/xApi.png" alt=""></center>
+    </div>
+  </section>
+
+<?php
+require_once("footer.html");
+?>
+</body>
+</html>
